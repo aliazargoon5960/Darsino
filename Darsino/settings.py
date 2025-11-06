dec10f57c568fecaa63e2e7ef8b9cce7be36eb35
AliAsghar Azargoon <aliasgharazargoon5960@gmail.com>
2025-11-06 23:17:14 +0330

    OK

--- Darsino/settings.py
+++ Darsino/settings.py
@@ -152,9 +152,9 @@ AUTH_USER_MODEL = 'account_module.User'
 
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
+    'CLOUD_NAME': "devswomrd",
+    'API_KEY': "799598748177243",
+    'API_SECRET': "6g4cn9wCbsb4qaswk5OzW_lHKxg"
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
